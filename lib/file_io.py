4f15b9190afcdd5b0d194097bae883660318c71d
Brett de Bear <bkdebear@gmail.com>
2023-02-27 18:59:12 -0800

    finished

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as temp:
+        temp.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as temp:
+        temp.write(append_content)
 
 def read_file(file_name):
-    pass
+    text_file = open(f'{file_name}.txt')
+    return text_file.read()
